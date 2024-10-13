fbac37577130fd624d813f79da430627a52b3524
Mauro T Barone <mautot.barone@gmail.com>
2024-10-13 16:31:07 +0200

    V1.0

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express= require('express');
 const path= require('path');
-const port=process.env.PORT||8000
+const PORT=process.env.PORT||8000;
 
-const app=express()
+const app=express();
 
 // setup static folder
 
@@ -22,20 +22,36 @@ app.get('/about',(req,res)=> {
         {id:1, title: 'Post One'},
         {id:2, title: 'Post Two'},
         {id:3, title: 'Post Three'},
-    ]
+    ];
+
+// get all posts
+app.get('/api/posts',(req,res)=>{
+    const limit=parseInt(req.query.limit);
+
+    if(!isNaN(limit)&&limit>0){
+       return res.status(200).json(posts.slice(0, limit));
+    }
+})
 
     //post è il nostro endpoint
     app.get('/api/posts/:id', (req, res)=>{
         const id = parseInt(req.params.id);
         console.log(id);
-        //res.json(posts.filter((post)=> post.id===id));
-        
+        const post=posts.find((post) => post.id ===id)
+        //res.status(200).json(posts.filter((post)=> post.id===id);
+
+        if (!post) {
+            return res.status(404).json({msg:` post with id of ${id} non esiste!`});
+
+        } 
+        res.status(200).json(post)
+
         //posta solo il title in base all'ID
-        //res.send(posts[id-1].title)
+        //res.send(posts[id-1].title);
     
         
     })
 
 
 
-app.listen(port, () => console.log(`server is running on port ${port}`))
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
